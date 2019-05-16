bbccd8b916ae84bcdb1f805c19abb8321976e10d
snailoff <snailoff@37point.com>
2019-05-16 18:41:10 +0900

    프로젝트에서 쓰는 특수문자 빈도 체크. 쿼트와 마이너스 승격. 아스테리커는 원래 짝인 8자리로 넣었다.

--- keymap.c
+++ keymap.c
@@ -37,7 +37,7 @@ enum planck_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/*
+/* 
  * Qwerty - snailoff
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
@@ -46,14 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |      | Alt  |Lower |   *  |    Space    |   _  |Raise |   '  |      | GUI  |
+ * | Ctrl |      | Alt  |Lower |   '  |    Space    |   -  |Raise |  GUI |      | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,	KC_Q,    KC_W,    KC_E,	 KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  	KC_A,    KC_S,    KC_D,	 KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, 	KC_Z,    KC_X,    KC_C,	 KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, XXXXXXX, KC_LALT,	  LOWER, KC_ASTR, KC_SPC,  KC_SPC,  KC_UNDS, RAISE,   KC_QUOT, XXXXXXX, KC_LGUI
+    KC_LCTL, XXXXXXX, KC_LALT,	  LOWER, KC_QUOT, KC_SPC,  KC_SPC,  KC_MINS, RAISE,   KC_LGUI, XXXXXXX, KC_LCTL
 ),
 
 /*
@@ -63,35 +63,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|  
  * | Pg Dn|      | Left | Down |Right |      |      |  4   |  5   |  6   |      | End  |  
  * |------+------+------+------+------+------+------+------+------+------+------+------|  
- * |------|      |   (  |   =  |   )  |  -   |  +   |  1   |  2   |  3   |      |------|  
+ * |------|      |   (  |   =  |   )  |  +   |      |  1   |  2   |  3   |      |------|  
  * |------+------+------+------+------+------+------+------+------+------+------+------|  
- * |------|      |      |------|      |     Del     |  0   |------|      |      |      |  
+ * |------|      |      |------|      |     Del     |  0   |------|      |------|------|  
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_PGUP, XXXXXXX, KC_LBRC, KC_UP,   KC_RBRC, XXXXXXX, XXXXXXX, KC_7,    KC_8,    KC_9, XXXXXXX, KC_HOME,
     KC_PGDN, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_4,    KC_5,    KC_6, XXXXXXX, KC_END,
-    _______, XXXXXXX, KC_LPRN, KC_EQL,  KC_RPRN, KC_MINS, KC_PLUS, KC_1,    KC_2,    KC_3, XXXXXXX, _______,
-    _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, KC_DEL,  KC_DEL,  KC_0, _______, XXXXXXX, XXXXXXX, XXXXXXX 
+    _______, XXXXXXX, KC_LPRN, KC_EQL,  KC_RPRN, KC_PLUS, XXXXXXX, KC_1,    KC_2,    KC_3, XXXXXXX, _______,
+    _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, KC_DEL,  KC_DEL,  KC_0, _______, XXXXXXX, _______, _______
 ),
 
 /*  
  * Raise - snailoff  
  * ,-----------------------------------------------------------------------------------.  
- * | Play |  F9  |  F10 |  F11 |  F12 |      |      |  &   |      |  `   |      | Vol+ |  
+ * | Play |  F9  |  F10 |  F11 |  F12 |      |      |  &   |  *   |  `   |      | Vol+ |  
  * |------+------+------+------+------+------+------+------+------+------+------+------|  
  * | Next |  F5  |  F6  |  F7  |  F8  |      |      |  $   |  %   |  ^   |      | Vol- |  
  * |------+------+------+------+------+------+------+------+------+------+------+------|  
  * |------|  F1  |  F2  |  F3  |  F4  |  |   |  ~   |  !   |  @   |  #   |      |------|  
  * |------+------+------+------+------+------+------+------+------+------+------+------|  
- * |------|      |      |------|  \   |     Del     |      |------|      |      |      |  
+ * |------|      |      |------|  \   |     Del     |      |------|      |------|------|  
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_MPLY, KC_F9,   KC_F10, KC_F11,	 KC_F12,  XXXXXXX, XXXXXXX, KC_AMPR, XXXXXXX, KC_GRV,  XXXXXXX, KC_VOLU,
+    KC_MPLY, KC_F9,   KC_F10, KC_F11,	 KC_F12,  XXXXXXX, XXXXXXX, KC_AMPR, KC_ASTR, KC_GRV,  XXXXXXX, KC_VOLU,
     KC_MNXT, KC_F5,   KC_F6,  KC_F7,	 KC_F8,   XXXXXXX, XXXXXXX, KC_DLR,  KC_PERC, KC_CIRC, XXXXXXX, KC_VOLD,
     _______, KC_F1,   KC_F2,  KC_F3,	 KC_F4,   KC_PIPE, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, XXXXXXX, _______, 
-    _______, XXXXXXX, XXXXXXX,  _______, KC_BSLS, KC_DEL,  KC_DEL,  XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX
+    _______, XXXXXXX, XXXXXXX,  _______, KC_BSLS, KC_DEL,  KC_DEL,  XXXXXXX, _______, XXXXXXX, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
