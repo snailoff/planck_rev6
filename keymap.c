9087f0c041d0caec2ba3ba901c562b5875a294ae
snailoff <snailoff@gmail.com>
2019-06-25 19:07:53 +0900

    quot 와 괄호가 가까이 있으니 조합이 좋지아니하여 mins와 자리 바꿈.

--- keymap.c
+++ keymap.c
@@ -46,14 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |      | Alt  |Lower |   '  |    Space    |   -  |Raise |  GUI |      | Ctrl |
+ * | Ctrl |      | Alt  |Lower |   -  |    Space    |   '  |Raise |  GUI |      | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_ESC,	KC_Q,    KC_W,    KC_E,	 KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  	KC_A,    KC_S,    KC_D,	 KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
     KC_LSFT, 	KC_Z,    KC_X,    KC_C,	 KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, XXXXXXX, KC_LALT,	  LOWER, KC_QUOT, KC_SPC,  KC_SPC,  KC_MINS, RAISE,   KC_LGUI, XXXXXXX, KC_LCTL
+    KC_LCTL, XXXXXXX, KC_LALT,	  LOWER, KC_MINS, KC_SPC,  KC_SPC,  KC_QUOT, RAISE,   KC_LGUI, XXXXXXX, KC_LCTL
 ),
 
 /*
